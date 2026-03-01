a41c046525e44933d807f2738da51baa4cfee7f5
matanyaAlkobi <198562436+matanyaAlkobi@users.noreply.github.com>
2026-03-01 18:40:11 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-import express from "express";
-import userRoutes from './router/users.js'
-import 'dotenv/config'
-const server = express();
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { Resource } from '@opentelemetry/resources';
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
-server.use(express.json());
-server.use('/users',userRoutes)
-server.listen(process.env.EXSPRESS_PORT, () => {
-  console.log("serverlistening...");
+const exporter = new OTLPTraceExporter({
+  url: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
+    ?? `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT ?? 'http://localhost:4318'}/v1/traces`,
 });
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME ?? 'my-nest-service',
+  }),
+  traceExporter: exporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start()
