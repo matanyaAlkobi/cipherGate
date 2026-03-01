6bedafbedca733fa9b012da829b62db1297cb5b3
matanyaAlkobi <198562436+matanyaAlkobi@users.noreply.github.com>
2026-03-01 19:13:54 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
+// tracing.ts
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { Resource } from '@opentelemetry/resources';
-import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
 const exporter = new OTLPTraceExporter({
-  url: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
-    ?? `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT ?? 'http://localhost:4318'}/v1/traces`,
+  url:
+    process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT ??
+    `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT ?? 'http://localhost:4318'}/v1/traces`,
 });
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME ?? 'my-nest-service',
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: process.env.OTEL_SERVICE_NAME ?? 'my-nest-service',
   }),
   traceExporter: exporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
-sdk.start()
+sdk.start();
